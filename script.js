089b7a03ccd84e7fe92fab32bb228b74c63fdcba
Sushant Bhutani <sb.bhutani12@gmail.com>
2023-06-30 00:41:51 +0530

    Events fixed

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-const toggleClassOfStars = (id, isMouseInEvent) => {
-  for (let i = 1; i <= id; i++) {
+const toggleClassOfStars = (addTill, count) => {
+  for (let i = 1; i <= count; i++) {
     const star = document.getElementById(i);
-    if (isMouseInEvent) {
-      star.classList.add("fa-star");
-      star.classList.remove("fa-star-o");
-    } else {
-      star.classList.remove("fa-star");
-      star.classList.add("fa-star-o");
+
+    if (star) {
+      if (addTill >= i) {
+        star.classList.add("fa-star");
+        star.classList.remove("fa-star-o");
+      } else {
+        star.classList.remove("fa-star");
+        star.classList.add("fa-star-o");
+      }
     }
   }
 };
 
 function Star(el, count, callback) {
-  // Write a logic to create star rating problem
-  let stars = "";
-  for (let i = 1; i <= count; i++) {
-    stars = stars + `<i class="fa fa-star-o" id=${i}></i>`;
-  }
   const starsContainer = document.querySelector(el);
+  let selectedStarCount = 0;
+  for (let i = 1; i <= count; i++) {
+    const star = document.createElement("i");
+    star.className = "fa fa-star-o";
+    star.id = i;
+    starsContainer.appendChild(star);
+  }
 
-  const onMouseOut = (event) => {
-    toggleClassOfStars(event.target.id, false);
+  const onMouseOut = () => {
+    toggleClassOfStars(selectedStarCount, count);
   };
 
   const onMouseOver = (event) => {
-    toggleClassOfStars(event.target.id, true);
+    toggleClassOfStars(event.target.id, count);
   };
 
   const onMouseClick = (event) => {
-    toggleClassOfStars(event.target.id, true);
-    starsContainer.removeEventListener("mouseout", onMouseOut);
-    starsContainer.removeEventListener("mouseover", onMouseOver);
-    starsContainer.removeEventListener("click", onMouseClick);
-    callback(event.target.id);
+    selectedStarCount = event.target.id;
+    callback(selectedStarCount);
   };
 
   starsContainer.addEventListener("mouseout", onMouseOut);
   starsContainer.addEventListener("mouseover", onMouseOver);
   starsContainer.addEventListener("click", onMouseClick);
-
-  starsContainer.innerHTML = stars;
 }
 
 function getStar(value) {
